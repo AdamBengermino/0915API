b14e040ef30b1b469b401d51951fb2578a0b30fd
Eric Schwartz <support@addteq.com>
2016-09-15 04:22:19 -0400

    initial commit

---
+++ api/models/Employee.js
@@ -0,0 +1,46 @@
+/**
+ * Employee.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+module.exports = {
+  attributes: {
+
+  	firstName: {
+	    type: 'string',
+	    required: true,
+	    minLength: 2
+  	},
+  	lastName: {
+	    type: 'string',
+	    required: true,
+	    minLength: 2
+  	},
+  	email: {
+	    type: 'string',
+	    unique: true,
+	    required: true,
+	    email: true
+  	},
+  	homePhone: {
+  		type: 'string',
+  		regex: /^\(?([0-9]{3})\)?[- ]?([0-9]{3})[- ]?([0-9]{4})$/
+  	},
+  	cellPhone: {
+  		type: 'string',
+  		regex: /^\(?([0-9]{3})\)?[- ]?([0-9]{3})[- ]?([0-9]{4})$/
+  	},
+  	password: {
+  		type: "string",
+  		required: true,
+  		regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^\&*\)\(+=._-])[0-9a-zA-Z!@#\$%\^\&*\)\(+=._-]{8,}$/
+  	},
+  	active: {
+	    type: 'int',
+	    enum: [1,0],
+	    required: true
+  	}
+  }
+};
+
